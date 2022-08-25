408d5aee9b23faf9b130b48e64b222bbd33c7447
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-08-25 15:09:59 +0300

    Updates

--- src/components/header-button/HeaderButton.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './headerButton.module.css';
-
-export default function HeaderButton() {
-    return (
-        <a href='#feedback-form' className={styles.button}>Постучаться</a>
-    )
-}
--- src/components/secret/Secret.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './secret.module.css'
-
-export default function Secret() {
-    return (
-        <section className={styles.secret}>
-            <p className={styles.paragraph}>Если не спросить — никогда не узнаешь, если знаешь — нужно лишь спросить.</p>
-        </section>
-    )
-}
