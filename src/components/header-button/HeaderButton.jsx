f0c78951ee97acca4affc5f882263df715c45576
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-09-16 18:28:41 +0300

    utm api

--- src/components/header-button/HeaderButton.jsx
+++ src/components/header-button/HeaderButton.jsx
@@ -2,6 +2,6 @@ import styles from './headerButton.module.css';
 
 export default function HeaderButton() {
     return (
-        <div href='#feedback-form' className={styles.button}>Постучаться</div>
+        <a href='#feedback-form' className={styles.button}>Постучаться</a>
     )
 }
